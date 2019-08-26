dac823740585eed9854cb45efe75babc8a04d156
Ionela Palacean <ione.caia85@gmail.com>
2019-08-26 21:33:03 +0300

    studied classes, variables, primitive, data type and GGREGATION

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -6,8 +6,42 @@ package org.fasttrackit;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static  void main(String[] args )
     {
-        System.out.println( "Hello World!" );
+        // this is the one-line comment
+
+       Car car= new Car();
+       car.name="Dacia";
+       car.fuelLevel=60;
+       car.mileage=8.5;
+       car.doorCount=5;
+       car.maxSpeed=180;
+       car.fuelType="Gasoline";
+
+       Engine engine=new Engine();
+       engine.manufacturer="Renault";
+       engine.capacity=1600;
+
+       car.engine=engine;
+        System.out.println(car.name);
+        System.out.println(car.travelDistance);
+        System.out.println(car.running);
+        System.out.println(car.engine);
+
+        Car car2= new Car();
+        car2.name="Golf";
+        car.fuelLevel=70;
+        car.mileage=10;
+        car.doorCount=2;
+        car.maxSpeed=210;
+        car.fuelType="Diesel";
+
+        car.name=null;
+//concatenation
+        System.out.println("car2 name: "+ car2.name);
+        System.out.println("car1 name: "+ car.name);
+
+        car=car2;
+
     }
 }
---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,15 @@
+package org.fasttrackit;
+
+public class Car {
+
+    String name;
+    double mileage;
+    double maxSpeed;
+    String fuelType;
+    double fuelLevel;
+    double travelDistance;
+    int doorCount;
+    boolean running;
+    //has-a relationship
+    Engine engine;
+}
