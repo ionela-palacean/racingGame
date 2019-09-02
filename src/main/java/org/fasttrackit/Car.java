c44f1bef86f2f5ed56fecd3b8d8acbecbf83d461
Ionela Palacean <ione.caia85@gmail.com>
2019-09-02 19:06:33 +0300

    studied classes, variables, primitive, data type and GGREGATION

--- src/main/java/org/fasttrackit/Car.java
+++ src/main/java/org/fasttrackit/Car.java
@@ -1,15 +1,11 @@
 package org.fasttrackit;
+// inheritance (is-a relationship)
+public class Car extends AutoVehicle {
 
-public class Car {
-
-    String name;
-    double mileage;
-    double maxSpeed;
-    String fuelType;
-    double fuelLevel;
-    double travelDistance;
     int doorCount;
-    boolean running;
-    //has-a relationship
-    Engine engine;
+
+
+
+    // is-a
+
 }
