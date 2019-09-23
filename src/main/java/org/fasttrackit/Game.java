302278939b1a601530bef97821a7d1f375af3e07
Ionela Palacean <ione.caia85@gmail.com>
2019-09-23 20:08:59 +0300

    studied try-catch blocks

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -2,55 +2,74 @@ package org.fasttrackit;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Game {
 
     private Vehicle competitor1;
     private Vehicle competitor2;
-    private Track[] tracks= new Track[3];
-    private List<Vehicle> competitors= new ArrayList<>();
+    private Track[] tracks = new Track[3];
+    private List<Vehicle> competitors = new ArrayList<>();
 
 
+    public void start() {
 
-public void start () {
-
-    initializeTracks();
-    displayTracks();
-    initializeCompetitors();
-
-}
-
-private void initializeCompetitors(){
-    int competitorCount=2;
-    System.out.println(" Today's competitors are:");
-    for (int i=0; i<competitorCount;i++) {
-
-        Vehicle competitor= new Vehicle();
-        competitor.setName("Competitor " + i);
-        competitor.setMaxSpeed(300);
-        competitor.setMileage(ThreadLocalRandom.current().nextDouble(6,12));
-        competitor.setFuelLevel(80);
-
-        System.out.println(competitor);
-
-        competitors.add(competitor);
-
+        initializeTracks();
+        displayTracks();
+        initializeCompetitors();
 
     }
-}
 
-private void displayTracks() {
+    private void initializeCompetitors() {
+        int competitorCount = getCompetitorCountFromUser();
+        System.out.println(" Today's competitors are:");
+        for (int i = 0; i < competitorCount; i++) {
 
-    System.out.println("Available tracks: ");
+            Vehicle competitor = new Vehicle();
+            competitor.setName("Competitor " + i);
+            competitor.setMaxSpeed(300);
+            competitor.setMileage(ThreadLocalRandom.current().nextDouble(6, 12));
+            competitor.setFuelLevel(80);
 
-    for (int i=0; i<tracks.length; i++ ) {
-        if (tracks[i]!=null) {
-            System.out.println(tracks[i].getName() + "- Length: " + tracks[i].getLength() + " km.");
+            System.out.println(competitor);
+
+            competitors.add(competitor);
+
+
         }
     }
-}
+
+    private void displayTracks() {
+
+        System.out.println("Available tracks: ");
+
+        for (int i = 0; i < tracks.length; i++) {
+            if (tracks[i] != null) {
+                System.out.println(tracks[i].getName() + "- Length: " + tracks[i].getLength() + " km.");
+            }
+        }
+    }
+
+    private int getCompetitorCountFromUser() {
+
+        System.out.println("Please enter number of players");
+        Scanner scanner=new Scanner(System.in);
+        try {
+
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+
+            throw new RuntimeException("You have entered an invalid number");
+        } finally {
+            System.out.println("Finally block is always executed.");
+        }
+        }
+
+
+
 private void initializeTracks() {
     Track track1 = new Track();
     track1.setName("Monte Carlo");
